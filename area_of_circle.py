27fbc39d726b8466a999dc5cb771c7c9838bdddd
Mukul Tiwari <142297241+mukulntiwari@users.noreply.github.com>
2023-09-16 13:01:59 +0530

    area_of_circle.py

--- area_of_circle.py
+++ area_of_circle.py
@@ -1,11 +1,3 @@
-'''
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-'''
 radius = float(input ("Input the radius of the circle : "))
 Area = str(22/7 * r**2);
 print ("The area of the circle with radius " + str(radius) + " is: " + Area)
